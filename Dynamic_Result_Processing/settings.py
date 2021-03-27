b0c7cfaee1387539ee0b94ab6eb209be3cc0135b
HussainShafayet <42945427+HussainShafayet@users.noreply.github.com>
2021-03-27 13:14:00 +0600

    make it better

--- Dynamic_Result_Processing/settings.py
+++ Dynamic_Result_Processing/settings.py
@@ -91,7 +91,7 @@ WSGI_APPLICATION = 'Dynamic_Result_Processing.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'Result_processing_database',
         'USER': 'postgres',
         'PASSWORD': 'psql123',
